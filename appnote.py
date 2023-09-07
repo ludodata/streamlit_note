a9f20751e5c7e0eeaba5e4b6d32a1f281bb0564d
ludodata <ludo.cancy@gmail.com>
2023-09-07 11:05:13 +0200

    modif matplot

--- appnote.py
+++ appnote.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
